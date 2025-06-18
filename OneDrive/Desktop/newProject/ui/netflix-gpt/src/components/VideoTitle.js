81e8dc9559e03c6a3ae7635cc55fb1dc44fb5561
[Sushil-vm] <[sushilkumarvm187@gmail.com]>
2025-06-18 12:55:07 +0530

    Secondary Container with movie List

--- OneDrive/Desktop/newProject/ui/netflix-gpt/src/components/VideoTitle.js
+++ OneDrive/Desktop/newProject/ui/netflix-gpt/src/components/VideoTitle.js
@@ -4,7 +4,7 @@ const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video pt-[20%] px-24 absolute  text-white bg-gradient-to-r from-black ">
       <h1 className="text-6xl font-bold text-white">{title}</h1>
-      <p className="py-6 text-lg w-1/4  text-white ">{overview}</p>
+      <p className="py-6 text-base w-1/4  text-white ">{overview}</p>
       <div className="">
         <button className="bg-white text-black p-4 px-12 text-lg rounded-md hover:bg-opacity-50">
           ▶ Play
---
+++ OneDrive/Desktop/newProject/ui/netflix-gpt/src/hooks/useTrendingMovies.js
@@ -0,0 +1,28 @@
+
+
+
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { useEffect } from "react";
+import {   addTrendingMovies, } from "../utils/movieSlice";
+
+const useTrendingMovies = () => {
+  // Fetch dat from TMDB API and update the store
+  const dispatch = useDispatch();
+   
+  const getTrendingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/trending/movie/day?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    // console.log(json.results);
+    dispatch(addTrendingMovies(json.results));
+  };  
+
+  useEffect(() => {
+    getTrendingMovies();
+  }, []);
+};
+
+export default useTrendingMovies;
