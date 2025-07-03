f10f286f5f81f562294cbedd3fff0b09d1eb125b
[Sushil-vm] <[sushilkumarvm187@gmail.com]>
2025-07-03 15:51:07 +0530

    Added multiple pages for navbar items and implemented root-level authentication in Body.js

--- src/components/browse/MainContainer.js
+++ src/components/browse/MainContainer.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import VideoTitle from "../video/VideoTitle";
 import VideoBackground from "../video/VideoBackground";
 
 const MainContainer = () => {
-  const movies = useSelector((store) => store.movies?.nowPlayingMovies );
-  
-  if (!movies) return;
-  const mainMovie = movies[7];
-  const { id,original_title, overview } = mainMovie;
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  // const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
 
+  if (!movies) return null;
+
+  // useEffect(()=>{
+  //   const interval = setInterval
+  // })
+  // const mainMovie = movies[currentMovieIndex];
+  const mainMovie = movies[0];
+  const { id, original_title, overview } = mainMovie;
 
   return (
     <div className="pt-[30%] bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground  movieId={id}/>
+      <VideoBackground movieId={id} />
     </div>
   );
 };
--- src/components/video/VideoTitle.js
+++ src/components/video/VideoTitle.js
@@ -4,7 +4,9 @@ const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video pt-[20%] px-6 md:px-24 absolute  text-white bg-gradient-to-r from-black ">
       <h1 className="text-2xl md:text-6xl font-bold">{title}</h1>
-      <p className="hidden md:inline-block py-6 text-lg w-1/4  text-white ">{overview}</p>
+      <p className="hidden md:inline-block py-6 text-lg w-1/4  text-white ">
+        {overview}
+      </p>
       <div className="my-4 md:m-0">
         <button className="bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl rounded-lg hover:bg-opacity-50">
           ▶ Play
@@ -12,6 +14,10 @@ const VideoTitle = ({ title, overview }) => {
         <button className="hidden md:inline-block mx-2 bg-gray-400 text-white p-4 px-12 text-xl bg-opacity-80 rounded-md">
           ℹ️More Info
         </button>
+        <div className="absolute right-10 bottom-10 flex gap-4">
+          <button className="bg-gray-600 text-white p-2 rounded-full hover:bg-gray-500">◀</button>
+          <button className="bg-gray-600 text-white p-2 rounded-full hover:bg-gray-500">▶</button>
+        </div>
       </div>
     </div>
   );
